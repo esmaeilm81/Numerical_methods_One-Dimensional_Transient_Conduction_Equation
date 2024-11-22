17dfab90111b7c6eb4241bf9f382ef125b0b5b3c
esmaeilm81 <139987017+esmaeilm81@users.noreply.github.com>
2024-11-22 20:08:23 +0330

    Update Adv_CFD2_3.m

--- Adv_CFD2_3.m
+++ Adv_CFD2_3.m
@@ -10,6 +10,7 @@ t=v*h^2/alpha;
 tol=1e-9;
 maxit=N;
 
+A=zeros(N,N);
 B=zeros(N,N);
 phi_pp=zeros(N,1);
 phi_pp(1)=1;
@@ -17,16 +18,18 @@ phi_pp(N)=0;
 for j=1:N
    x(j)=(j-1)*h;
 end
-B(1,1)=1; B(N,N)=1;
+A(1,1)=1; A(N,N)=1;
 k=2;
 for j=2:N-1
-   B(k,j-1)=-2*v;
-   B(k,j)=4*v;
-   B(k,j+1)=-2*v;
+   A(k,j-1)=v;
+   A(k,j)=1-2*v;
+   A(k,j+1)=v;
    k=k+1;
 end
-B=sparse(B);
-phi_p=gmres(B,phi_pp,[],tol,maxit);
+A=sparse(A);
+phi_p=A*phi_pp;
+
+B(1,1)=1; B(N,N)=1;
 k=2;
 for j=2:N-1
    B(k,j-1)=2*v;
@@ -34,6 +37,8 @@ for j=2:N-1
    B(k,j+1)=2*v;
    k=k+1;
 end
+B=sparse(B);
+
 for i=1:10000000
     phi=B*phi_p+phi_pp;
     error(i)=max(abs(phi-phi_p));
